4a3a26284e9a775c0d35daaa2b5f738443cd029b
kath <kathleenbaltazar.slu@gmail.com>
2016-02-19 18:45:03 +0800

    Added Semester and SemesterTest class.

---
+++ src/test/java/com/orangeandbronze/enlistment/domain/SemesterTest.java
@@ -0,0 +1,30 @@
+package com.orangeandbronze.enlistment.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SemesterTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNegativeSemesterId() {
+		Student student = new Student(1);
+		Section section = new Section("A", new Schedule(Days.MTH, Period.H0830), new Room("S120", 25));
+		new Semester(-1, student, section);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testZeroSemesterId() {
+		Student student = new Student(1);
+		Section section = new Section("A", new Schedule(Days.MTH, Period.H0830), new Room("S120", 25));
+		new Semester(0, student, section);
+	}
+	
+	@Test
+	public void createValidSemester(){
+		Student student = new Student(1);
+		Section section = new Section("A", new Schedule(Days.MTH, Period.H0830), new Room("S120", 25));
+		new Semester(1, student, section);
+	}
+
+}
