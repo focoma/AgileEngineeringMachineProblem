f46f526af09aa8cc9d444c428d9bc06aaf34b16b
Dhon <dhonrandyoctavo@gmail.com>
2016-02-19 17:14:13 +0800

    Added SubjectTest.java

---
+++ src/test/java/com/orangeandbronze/enlistment/SubjectTest.java
@@ -0,0 +1,11 @@
+package com.orangeandbronze.enlistment;
+
+import org.junit.Test;
+
+public class SubjectTest {
+
+	@Test
+		public void SubjectNameIsAlphaNumeric() {
+			new Subject("Math101");
+	}
+}
