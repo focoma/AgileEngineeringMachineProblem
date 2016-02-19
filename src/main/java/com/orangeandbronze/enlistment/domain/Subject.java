54b9930a4d05068f1f69f3e35e1746b80fc749b6
Timothy Penarubia <mothypenarubia@gmail.com>
2016-02-19 18:52:48 +0800

    Subject

--- src/main/java/com/orangeandbronze/enlistment/domain/Subject.java
+++ src/main/java/com/orangeandbronze/enlistment/domain/Subject.java
@@ -1,12 +1,23 @@
 package com.orangeandbronze.enlistment.domain;
 
 enum Subject {
-    Math1, Math2(Math1);
-	
-    Subject(Subject preReq) {
-	   
-	}
-	Subject() {
-	}
+    None(null), Math1(null), Math2(Math1), Math3(Math2);
+	private final Subject preReq;
+	Subject(Subject preReq){
+	  this.preReq = preReq;
+    }
+		
+	//This will check if the Subject has Prerequisites and return boolean
+	public boolean checkIfSubjectHasPreRequisites(){
+		for(Subject subject: Subject.values()){;
 
+			if(subject.name().equals(preReq.name()) && subject.name()!=null){
+			 return true;
+			}
+		}
+		return false;
+	}
+	
 }
+
+
--- src/test/java/com/orangeandbronze/enlistment/domain/SubjectTest.java
+++ src/test/java/com/orangeandbronze/enlistment/domain/SubjectTest.java
@@ -3,12 +3,22 @@ package com.orangeandbronze.enlistment.domain;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import static com.orangeandbronze.enlistment.domain.Subject.*;
 public class SubjectTest {
 
 	@Test
-	public void SubjectTest() {
-		 
+	public void sameSubjectTest() {
+	assertEquals(Subject.Math1, Subject.Math1);
+	}
+	
+	@Test
+	public void preRequesitesSubjectTest() {
+		assertEquals(true, Math3.checkIfSubjectHasPreRequisites());
+	}
+	
+	@Test
+	public void preRequesitesSubjectTest1() {
+		assertEquals(false, Math1.checkIfSubjectHasPreRequisites());
 	}
 
 }
