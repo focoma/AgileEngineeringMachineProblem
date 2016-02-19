42c5827ce4a5076cf4704013dcaa8f6cbb002cc3
Jerry Sales <jerrysalesjr@gmail.com>
2016-02-19 17:29:37 +0800

    Modify subject exception and override toString, hashCode and equals

--- src/main/java/com/orangeandbronze/enlistment/Subject.java
+++ src/main/java/com/orangeandbronze/enlistment/Subject.java
@@ -11,7 +11,45 @@ public class Subject {
 	
 	void checkPreRequisites(Subject subj){
 		if(this.equals(subj)) {
-			throw new PreRequisiteSubjectRequiredException("The subject " + subj);
+			throw new PreRequisiteSubjectRequiredException("The subject " + subj + " has a prerequisite of subject " + preRequisites);
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((preRequisites == null) ? 0 : preRequisites.hashCode());
+		result = prime * result + ((subjectName == null) ? 0 : subjectName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Subject other = (Subject) obj;
+		if (preRequisites == null) {
+			if (other.preRequisites != null)
+				return false;
+		} else if (!preRequisites.equals(other.preRequisites))
+			return false;
+		if (subjectName == null) {
+			if (other.subjectName != null)
+				return false;
+		} else if (!subjectName.equals(other.subjectName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Subject [subjectName=" + subjectName + ", preRequisites=" + preRequisites + "]";
+	}
+	
+	
 }
