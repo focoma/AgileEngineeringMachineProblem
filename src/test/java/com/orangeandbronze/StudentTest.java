d254f7880eb96cee8f51211340da07d0fcddb81c
bertongputik <robertlouierodriguez@gmail.com>
2016-02-19 18:36:42 +0800

    fixed merge conflict from remote develop

--- src/test/java/com/orangeandbronze/StudentTest.java
+++ src/test/java/com/orangeandbronze/StudentTest.java
@@ -21,11 +21,26 @@ public class StudentTest {
         Section section = new Section(SECTION, new Schedule(Days.MTH, Period.H0830), new Room(ROOM103, 3));
     }
 
-//    @Test(expected = ScheduleConflictException.class)
-//    public void enlistSectionSameScheduleTest() {
-//        Student student = new Student(1);
-//        student.enlist(s1);
-//        student.enlist(s2);
-//    }
+    @Test(expected = ScheduleConflictException.class)
+    public void enlistSectionSameScheduleTest() {
+        Section s1 = new Section(SECTION, SCHEDULE,ROOM1);
+        Section s2 = new Section(SECTION2,SCHEDULE,ROOM1);
+        Student student = new Student(1);
+        student.enlist(s1);
+        student.enlist(s2);
+    }
+
+
+    @Test
+    public void enlistStudentWithoutExceedingCapacity(){
+        new Room("room1",3);
+        Student student = new Student(1);
+        Student student2 = new Student(2);
+        Student student3 = new Student(3);
+        Section section = new Section(SECTION, new Schedule(Days.MTH, Period.H0830), new Room(ROOM103, 3));
+        student.enlist(section);
+        student2.enlist(section);
+        student3.enlist(section);
+    }
 
 }
