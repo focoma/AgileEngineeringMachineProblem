90a93738f6db3396b24ace1ec91e0943f6e743c6
dhon <dhonrandyoctavo@gmail.com>
2016-02-20 14:13:47 +0800

    Declare import locations of files

--- src/main/java/com/orangeandbronze/enlistment/service/EnlistService.java
+++ src/main/java/com/orangeandbronze/enlistment/service/EnlistService.java
@@ -1,13 +1,10 @@
 package com.orangeandbronze.enlistment.service;
 
-import static com.orangeandbronze.enlistment.Defaults.*;
-import com.orangeandbronze.enlistment.Room;
-import com.orangeandbronze.enlistment.Section;
-import com.orangeandbronze.enlistment.Student;
-import com.orangeandbronze.enlistment.Subject;
-import com.orangeandbronze.enlistment.dao.SectionDAO;
-import com.orangeandbronze.enlistment.dao.StudentDAO;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+import com.orangeandbronze.enlistment.Defaults;
+import com.orangeandbronze.enlistment.*;
+import com.orangeandbronze.enlistment.dao.*;
 
 public class EnlistService {
 	
@@ -16,7 +13,7 @@ public class EnlistService {
 	
 	public void enlist(int studentNo, String sectionId) {
 		final Student student = new Student(studentNo);
-	    final Section section = new Section(sectionId, SCHEDULE, new Room("GDO101",2), new Subject("Math"), 1);
+	    final Section section = new Section(sectionId, Defaults.SCHEDULE, new Room("GDO101",2), new Subject("Math"), 1);
 	    
 		when(studentDao.getStudentNo(studentNo)).thenReturn(student);
 		when(sectionDao.getSectionId(sectionId)).thenReturn(section);
