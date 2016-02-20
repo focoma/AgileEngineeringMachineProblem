5d5e6fda2739b56cd4abdba895856f162345bb5b
renzmiller <renzaldamamiller@gmail.com>
2016-02-20 09:13:07 +0800

    Removed duplicate tests for RoomTest and StudentTest

--- src/test/java/com/orangeandbronze/RoomTest.java
+++ src/test/java/com/orangeandbronze/RoomTest.java
@@ -26,46 +26,4 @@ public class RoomTest {
     public void checkAlphanumeric() {
         new Room(ROOM101, 2);
     }
-
-    @Test(expected = RoomCapacityException.class)
-    public void roomExceedsCapacity() {
-        Student student1 = new Student(001);
-        Student student2 = new Student(002);
-        Student student3 = new Student(003);
-        Student student4 = new Student(004);
-
-        Section section1 = new Section("Section1", SCHEDULE_H0830 , new Room(ROOM101, 4), SUBJECT_MATH_01, S_FIRST_SEM);
-        Section section2 = new Section("Section2", SCHEDULE_H1000, new Room(ROOM102, 3), SUBJECT_MATH_02, S_FIRST_SEM);
-
-        student1.enlist(section1);
-        student2.enlist(section1);
-        student3.enlist(section1);
-        student4.enlist(section1);
-
-        student1.enlist(section2);
-        student2.enlist(section2);
-        student3.enlist(section2);
-        student4.enlist(section2);
-    }
-
-    @Test
-    public void roomDoesNotExceedsCapacity() {
-        Student student1 = new Student(001);
-        Student student2 = new Student(002);
-        Student student3 = new Student(003);
-        Student student4 = new Student(004);
-
-        Section section1 = new Section("Section1", SCHEDULE_H0830 , new Room(ROOM101, 3), SUBJECT_MATH_01, S_FIRST_SEM);
-        Section section2 = new Section("Section2", new Schedule(Days.TF, Period.H0830), new Room(ROOM101, 4), SUBJECT_MATH_02, S_FIRST_SEM);
-
-        student1.enlist(section1);
-        student2.enlist(section1);
-        student3.enlist(section1);
-
-        student1.enlist(section2);
-        student2.enlist(section2);
-        student3.enlist(section2);
-        student4.enlist(section2);
-    }
-
 }
--- src/test/java/com/orangeandbronze/StudentTest.java
+++ src/test/java/com/orangeandbronze/StudentTest.java
@@ -33,68 +33,55 @@ public class StudentTest {
     }
 
     @Test(expected = ScheduleConflictException.class)
-    public void enlistSectionSameScheduleTest() {
-        Section s1 = new Section(SECTION1_NAME, SCHEDULE_H0830 ,ROOM1_5, SUBJECT_MATH_01, S_FIRST_SEM);
-        Section s2 = new Section(SECTION2_NAME,SCHEDULE_H0830 ,ROOM1_5, SUBJECT_MATH_01, S_FIRST_SEM);
+    public void enlistStudentWithConflict() {
+        Section s1 = new Section(SECTION1_NAME, SCHEDULE_H0830, ROOM1_5, SUBJECT_MATH_01, S_FIRST_SEM);
+        Section s2 = new Section(SECTION2_NAME, SCHEDULE_H0830, ROOM1_5, SUBJECT_MATH_01, S_FIRST_SEM);
         Student student = new Student(1);
         student.enlist(s1);
         student.enlist(s2);
     }
 
     @Test
-    public void enlistStudentWithoutExceedingCapacityFull(){
+    public void enlistStudentWithoutExceedingCapacity() {
         Student student = new Student(1);
         Student student2 = new Student(2);
-        
+
         Section section = new Section(SECTION1_NAME, new Schedule(Days.MTH, Period.H0830), new Room(ROOM103, 3), SUBJECT_MATH_01, S_FIRST_SEM);
         student.enlist(section);
         student2.enlist(section);
     }
-    
-    @Test
-    public void enlistStudentWithoutExceedingCapacityNotFull(){
+
+    @Test(expected = RoomCapacityException.class)
+    public void enlistStudentExceededCapacity() {
+        Section section = new Section(SECTION3_NAME, SCHEDULE_H0830, ROOM2_2, SUBJECT_MATH_01, S_FIRST_SEM);
         Student student = new Student(1);
         Student student2 = new Student(2);
         Student student3 = new Student(3);
-        
-        Section section = new Section(SECTION1_NAME, new Schedule(Days.MTH, Period.H0830), new Room(ROOM103, 3), SUBJECT_MATH_01, S_FIRST_SEM);
         student.enlist(section);
         student2.enlist(section);
         student3.enlist(section);
     }
-    
-    @Test(expected = RoomCapacityException.class)
-    public void enlistStudentExceededCapacity(){
-    	Section section = new Section(SECTION3_NAME, SCHEDULE_H0830, ROOM2_2, SUBJECT_MATH_01, S_FIRST_SEM);
-        Student student = new Student(1);
-        Student student2 = new Student(2);
-        Student student3 = new Student(3);
-        student.enlist(section);
-        student2.enlist(section);
-        student3.enlist(section);  
-    }
-    
+
     @Test(expected = ExistingSubjectException.class)
     public void checkIfSubjectIsAlreadyAddedToAnyOfTheAddedSection() {
-    	Section section = new Section(SECTION3_NAME, SCHEDULE_H0830, ROOM2_2, SUBJECT_MATH_01, S_FIRST_SEM);
-    	Section section2 = new Section(SECTION2_NAME, SCHEDULE_H1000, ROOM2_2, SUBJECT_MATH_01, S_FIRST_SEM);
-        
+        Section section = new Section(SECTION3_NAME, SCHEDULE_H0830, ROOM2_2, SUBJECT_MATH_01, S_FIRST_SEM);
+        Section section2 = new Section(SECTION2_NAME, SCHEDULE_H1000, ROOM2_2, SUBJECT_MATH_01, S_FIRST_SEM);
+
         Student student = new Student(1);
         student.enlist(section);
         student.enlist(section2);
     }
-    
+
     // Test for prerequisite
     @Ignore
     public void test() {
-    	Section section = SECTION1;
-    	Section section2 = SECTION1_1;
-        
+        Section section = SECTION1;
+        Section section2 = SECTION1_1;
+
         Student student = new Student(1);
         student.enlist(section);
         student.enlist(section2);
     }
-    
-    
+
 
 }
