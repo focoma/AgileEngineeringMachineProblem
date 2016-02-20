0c3353da9923a8d97a6ea95f3bb4aeafba697757
renzmiller <renzaldamamiller@gmail.com>
2016-02-20 11:02:59 +0800

    Added 1 test for ScheduleTest: scheduleWithoutConflict

--- src/test/java/com/orangeandbronze/ScheduleTest.java
+++ src/test/java/com/orangeandbronze/ScheduleTest.java
@@ -1,16 +1,23 @@
 package com.orangeandbronze;
 
 
+import org.junit.Test;
+
 import static com.orangeandbronze.Default.*;
 import static com.orangeandbronze.SemesterEnum.S_FIRST_SEM;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Ignore;
-import org.junit.Test;
 public class ScheduleTest {
 
+    @Test
+    public void scheduleWithoutConflict(){
+        Section s1 = new Section(SECTION1_NAME, SCHEDULE_H0830, ROOM1_5, SUBJECT_MATH_01, S_FIRST_SEM);
+        Section s2 = new Section(SECTION2_NAME, SCHEDULE_H1000, ROOM1_5, SUBJECT_MATH_02, S_FIRST_SEM);
+        Student student = new Student(1);
+        student.enlist(s1);
+        student.enlist(s2);
+    }
+
     @Test(expected = ScheduleConflictException.class)
-    public void enlistStudentWithConflict() {
+    public void scheduleWithConflict() {
         Section s1 = new Section(SECTION1_NAME, SCHEDULE_H0830, ROOM1_5, SUBJECT_MATH_01, S_FIRST_SEM);
         Section s2 = new Section(SECTION2_NAME, SCHEDULE_H0830, ROOM1_5, SUBJECT_MATH_01, S_FIRST_SEM);
         Student student = new Student(1);
